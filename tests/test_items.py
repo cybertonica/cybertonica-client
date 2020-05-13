ef717c843af31fd40d7b85a08aa838c1e47712e7
ochaplashkin <ochaplashkin@cybertonica.com>
2020-05-13 14:45:53 +0400

    AF-5654 added tests for Items

--- tests/test_items.py
+++ tests/test_items.py
@@ -24,7 +24,7 @@ class TestInitItemClass(unittest.TestCase):
 		self.assertIsInstance(self.items, Item)
 
 		self.assertTrue("get_all" in dir(self.items))
-		self.assertTrue("get_all_alive" in dir(self.items))
+		# self.assertTrue("get_all_alive" in dir(self.items))
 		self.assertTrue("get_by_id" in dir(self.items))
 		self.assertTrue("create" in dir(self.items))
 		self.assertTrue("update" in dir(self.items))
@@ -106,36 +106,36 @@ class TestGetByIDMethod(unittest.TestCase):
 		with self.assertRaisesRegex(AssertionError, 'The ID must not be an empty string'):
 			self.items.get_by_id(self.list_id, '')
 
-class TestGetAllAliveMethod(unittest.TestCase):
+# class TestGetAllAliveMethod(unittest.TestCase):
 
-	def setUp(self):
-		self.items = Item(PropertyMock(
-			url='test_url',
-			team='test_team',
-			signature='test_signature',
-			token='old_value',
-			verify=True,
-			r=Mock(return_value=(200, {'token': '123'}))
-		))
-		self.list_id = 'list_id'
+# 	def setUp(self):
+# 		self.items = Item(PropertyMock(
+# 			url='test_url',
+# 			team='test_team',
+# 			signature='test_signature',
+# 			token='old_value',
+# 			verify=True,
+# 			r=Mock(return_value=(200, {'token': '123'}))
+# 		))
+# 		self.list_id = 'list_id'
 
-	def test_get_all_alive_request(self):
-		self.items.get_all_alive(self.list_id)
-		self.items.root.r.assert_called_with(
-			'GET',
-			f'{self.items.root.url}/api/v1/items/{self.list_id}/alive',
-			body=None,
-			headers=None,
-			verify=self.items.root.verify
-		)
+# 	def test_get_all_alive_request(self):
+# 		self.items.get_all_alive(self.list_id)
+# 		self.items.root.r.assert_called_with(
+# 			'GET',
+# 			f'{self.items.root.url}/api/v1/items/{self.list_id}/alive',
+# 			body=None,
+# 			headers=None,
+# 			verify=self.items.root.verify
+# 		)
 
-	def test_get_all_alive_with_incorrect_list_id_type(self):
-		with self.assertRaisesRegex(AssertionError, 'List ID must be a string'):
-			self.items.get_all_alive(123)
+# 	def test_get_all_alive_with_incorrect_list_id_type(self):
+# 		with self.assertRaisesRegex(AssertionError, 'List ID must be a string'):
+# 			self.items.get_all_alive(123)
 	
-	def test_get_all_alive_with_empty_list_id_string(self):
-		with self.assertRaisesRegex(AssertionError, 'List ID must not be an empty string'):
-			self.items.get_all_alive('')
+# 	def test_get_all_alive_with_empty_list_id_string(self):
+# 		with self.assertRaisesRegex(AssertionError, 'List ID must not be an empty string'):
+# 			self.items.get_all_alive('')
 
 class TestCreateMethod(unittest.TestCase):
 
@@ -148,25 +148,34 @@ class TestCreateMethod(unittest.TestCase):
 			verify=True,
 			r=Mock(return_value=(200, {'token': '123'}))
 		))
+		self.list_id = 'test_id'
 		self.data = {'a': 1, 'b': 2}
 
 	def test_create_request(self):
-		self.items.create(self.data)
+		self.items.create(self.list_id, self.data)
 		self.items.root.r.assert_called_with(
 			'POST',
-			f'{self.items.root.url}/api/v1/items',
+			f'{self.items.root.url}/api/v1/items/{self.list_id}',
 			json.dumps(self.data),
 			headers=None,
 			verify=self.items.root.verify
 		)
 
+	def test_create_with_incorrect_list_id_type(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID type must be a string'):
+			self.items.create(123, self.data)
+	
+	def test_create_with_empty_list_id(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID must not be an empty'):
+			self.items.create('',self.data)
+
 	def test_create_with_incorrect_data_type(self):
 		with self.assertRaisesRegex(AssertionError, 'The data type must be a dictionary'):
-			self.items.create(123)
+			self.items.create(self.list_id, 123)
 	
 	def test_create_with_empty_dict(self):
 		with self.assertRaisesRegex(AssertionError, 'Item data must not be an empty dictionary'):
-			self.items.create({})
+			self.items.create(self.list_id,{})
 
 class TestUpdateMethod(unittest.TestCase):
 
@@ -179,34 +188,43 @@ class TestUpdateMethod(unittest.TestCase):
 			verify=True,
 			r=Mock(return_value=(200, {'token': '123'}))
 		))
+		self.list_id = 'test_id'
 		self.id = 'test_id'
 		self.data = {'a': 1, 'b': 2}
 
 	def test_update_request(self):
-		self.items.update(self.id, self.data)
+		self.items.update(self.list_id, self.id, self.data)
 		self.items.root.r.assert_called_with(
 			'PUT',
-			f'{self.items.root.url}/api/v1/items/{self.id}',
+			f'{self.items.root.url}/api/v1/items/{self.list_id}/{self.id}',
 			json.dumps(self.data),
 			headers=None,
 			verify=self.items.root.verify
 		)
 
+	def test_update_with_incorrect_list_id_type(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID must be a string'):
+			self.items.update(123, self.id, self.data)
+
+	def test_update_with_empty_list_id_string(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID must not be an empty string'):
+			self.items.update('',self.id, self.data)
+
 	def test_update_with_incorrect_id_type(self):
 		with self.assertRaisesRegex(AssertionError, 'The ID must be a string'):
-			self.items.update(123,self.data)
+			self.items.update(self.list_id, 123,self.data)
+
+	def test_update_with_empty_id_string(self):
+		with self.assertRaisesRegex(AssertionError, 'The ID must not be an empty string'):
+			self.items.update(self.list_id, '',self.data)
 	
 	def test_update_with_incorrect_data_type(self):
 		with self.assertRaisesRegex(AssertionError, 'The data type must be a dictionary'):
-			self.items.update(self.id,123)
-	
-	def test_update_with_empty_id_string(self):
-		with self.assertRaisesRegex(AssertionError, 'The ID must not be an empty string'):
-			self.items.update('',self.data)
+			self.items.update(self.list_id, self.id,123)
 	
 	def test_update_with_empty_data_dict(self):
 		with self.assertRaisesRegex(AssertionError, 'Item data must not be an empty dictionary'):
-			self.items.update(self.id,{})
+			self.items.update(self.list_id, self.id,{})
 
 class TestDeleteMethod(unittest.TestCase):
 
@@ -219,22 +237,31 @@ class TestDeleteMethod(unittest.TestCase):
 			verify=True,
 			r=Mock(return_value=(200, {'token': '123'}))
 		))
+		self.list_id = 'test_id'
 		self.id = 'test_id'
 
 	def test_delete_request(self):
-		self.items.delete(self.id)
+		self.items.delete(self.list_id, self.id)
 		self.items.root.r.assert_called_with(
 			'DELETE',
-			f'{self.items.root.url}/api/v1/items/{self.id}',
+			f'{self.items.root.url}/api/v1/items/{self.list_id}/{self.id}',
 			body=None,
 			headers=None,
 			verify=self.items.root.verify
 		)
 
+	def test_update_with_incorrect_list_id_type(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID must be a string'):
+			self.items.delete(123, self.id)
+	
+	def test_update_with_empty_list_id_string(self):
+		with self.assertRaisesRegex(AssertionError, 'List ID must not be an empty string'):
+			self.items.delete('', self.id)
+	
 	def test_update_with_incorrect_id_type(self):
 		with self.assertRaisesRegex(AssertionError, 'The ID must be a string'):
-			self.items.delete(123)
+			self.items.delete(self.list_id, 123)
 	
 	def test_update_with_empty_id_string(self):
 		with self.assertRaisesRegex(AssertionError, 'The ID must not be an empty string'):
-			self.items.delete('')
+			self.items.delete(self.list_id, '')
